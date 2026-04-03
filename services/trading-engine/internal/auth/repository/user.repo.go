010d8c2d29cf40e9e50ec4e5caef54138a674a29
Alemayehu <alemayehudabi606@gmail.com>
2026-04-03 15:32:24 +0300

    wip: create and findbyemail user-repo

--- services/trading-engine/internal/auth/repository/user.repo.go
+++ services/trading-engine/internal/auth/repository/user.repo.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/AlemayehuDabi/Smart_Sheild/services/trading-engine/internal/auth/model"
+	"gorm.io/gorm"
+)
 
 type UserRespository struct {
 	db *gorm.DB
@@ -11,8 +14,18 @@ func NewRespository(db *gorm.DB) *UserRespository {
 }
 
 // create new user
+func (r *UserRespository) Create (user *model.UserModel) error {
+	return r.db.Create(user).Error
+}
 
 // get user by email
+func (r *UserRespository) FindByEmail (email string) (*model.UserModel, error) {
+	var user model.UserModel
+
+	err := r.db.Where(user.Email).First(&user).Error
+
+	return &user, err
+}
 
 // get user by id
 
